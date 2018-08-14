0fe7b532fd8929c892d0de0d5f287b502cc75496
ono-hiroshi <hiroshi_ono_1989@yahoo.co.jp>
2018-08-15 01:18:25 +0900

    fix mysqlで動作するように変更

--- models.py
+++ models.py
@@ -3,10 +3,36 @@ from sqlalchemy.orm import (scoped_session, sessionmaker, relationship,
                             backref)
 from sqlalchemy.ext.declarative import declarative_base
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
+'''
 engine = create_engine('sqlite:///database.sqlite3', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
+'''
+DATABASE = 'mysql://%s:%s@%s/%s?charset=ujis' % (
+    "root",
+    "",
+    "127.0.0.1",
+    "sample",
+)
+
+ENGINE = create_engine(
+    DATABASE,
+    # encoding="euc-jp",
+    echo=True  # Trueだと実行のたびにSQLが出力される
+)
+
+# Sessionの作成
+db_session = scoped_session(
+    sessionmaker(
+        autocommit=False,
+        autoflush=False,
+        bind=ENGINE
+    )
+)
 
 Base = declarative_base()
 # We will need this for querying
@@ -15,6 +41,14 @@ Base.query = db_session.query_property()
 
 '''
 テーブル定義
+'''
+class Staff(Base):
+    __tablename__ = 'staff'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    branch = Column(String)
+    age = Column(String)
+
 '''
 class Department(Base):
     __tablename__ = 'department'
@@ -33,3 +67,4 @@ class Employee(Base):
         backref=backref('employees',
                         uselist=True,
                         cascade='delete,all'))
+'''
--- schema.py
+++ schema.py
@@ -1,19 +1,8 @@
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
-from models import db_session, Department as DepartmentModel, Employee as EmployeeModel
-
-
-class Department(SQLAlchemyObjectType):
-    class Meta:
-        model = DepartmentModel
-        interfaces = (relay.Node, )
-
-
-class Employee(SQLAlchemyObjectType):
-    class Meta:
-        model = EmployeeModel
-        interfaces = (relay.Node, )
+#from models import db_session, Department as DepartmentModel, Employee as EmployeeModel
+from models import db_session, Staff as StaffModel
 
 '''
 ■エンドポイント定義
@@ -41,10 +30,49 @@ class Employee(SQLAlchemyObjectType):
     }
   }
 }
-'''
+class Department(SQLAlchemyObjectType):
+    class Meta:
+        model = DepartmentModel
+        interfaces = (relay.Node, )
+
+class Employee(SQLAlchemyObjectType):
+    class Meta:
+        model = EmployeeModel
+        interfaces = (relay.Node, )
+
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
     all_employees = SQLAlchemyConnectionField(Employee)
     all_department= SQLAlchemyConnectionField(Department)
+
+schema = graphene.Schema(query=Query)
+'''
+
+
+'''
+■エンドポイント定義
+
+・リクエスト例
+{
+  allStaff {
+    edges {
+      node {
+        id
+        name
+        branch
+      }
+    }
+  }
+}
+'''
+class Staff(SQLAlchemyObjectType):
+    class Meta:
+        model = StaffModel
+        interfaces = (relay.Node, )
+
+class Query(graphene.ObjectType):
+    node = relay.Node.Field()
+    all_staff = SQLAlchemyConnectionField(Staff)
+    all_sample = SQLAlchemyConnectionField(Staff)
 
 schema = graphene.Schema(query=Query)
